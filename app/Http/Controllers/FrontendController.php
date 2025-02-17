75289c8975ddc8b3d163b2c1ef42af7a791e8be8
bellosillo137847 <jbellosillo.137847@gmail.com>
2025-02-17 11:40:27 +0800

    Initial Commit of MPI Project

---
+++ app/Http/Controllers/FrontendController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class FrontendController extends Controller
+{
+    public function index(){
+        return Inertia::render('Frontend/Home');
+    }
+
+    public function viewList(){
+        return Inertia::render('Frontend/ViewList');
+    }
+
+    public function manage(){
+        return Inertia::render('Frontend/Manage');
+    }
+
+    public function settings(){
+        return Inertia::render('Frontend/Settings');
+    }
+}
---
+++ resources/js/Pages/Frontend/ViewList.vue
@@ -0,0 +1,9 @@
+<template>
+    <Frontend>
+        <h1>This is the View List page</h1>
+    </Frontend>
+</template>
+
+<script setup>
+import Frontend from '@/Layouts/FrontendLayout.vue';
+</script>
