037c4370c5f0afd3c12a33e3f8a0a5a975d778c8
animatidus <animatidus@gmail.com>
2025-02-23 16:42:58 +0800

    Create store, update, destroy functions
    Create store, update, destroy functions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use App\Models\User;
 
@@ -9,6 +10,39 @@ class UserController extends Controller
 {
     public function index(){
         $user = User::all();
-        return response()->json($user);
+        return response()->json([
+            'status' => true,
+            'message' => 'Users retrieved successfully',
+            'data' => $user
+        ], 200);
+    }
+
+    public function show($id){
+        $user = User::find($id);
+        if(!empty($user)){
+            return response()->json([
+                'status' => true,
+                'message' => 'User found successfully',
+                'data' => $user
+            ], 200);
+        }else{
+            return response()->json([
+                'status' => false,
+                'message' => 'User not found'
+            ], 404);
+        }
+
+    }
+
+    public function store(Request $request){
+
+    }
+
+    public function update(Request $request, $id){
+
+    }
+
+    public function destroy($id){
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,7 @@ use Illuminate\Support\Facades\Route;
 // })->middleware('auth:sanctum');
 
 Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+Route::post('/users', [UserController::class, 'store']);
+Route::put('/users/{id}', [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
