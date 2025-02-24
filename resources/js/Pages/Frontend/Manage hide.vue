9fd446251d4820aa971b40eaf95781827333f223
bellosillo137847 <jbellosillo.137847@gmail.com>
2025-02-24 17:59:01 +0800

    Manage Section Update - Unfinished Layer table

---
+++ resources/js/Pages/Frontend/Manage hide.vue
@@ -0,0 +1,101 @@
+<template>
+    <Frontend>
+      <div class="px-[420px] py-[100px] container-fluid bg-gray-100">
+        <p class="font-bold">Upload Raw Data:</p>
+        <input type="file" accept=".txt" /><br><br><br><br><br>
+
+        <!-- Chart Container -->
+        <div class="w-2/3 bg-blue-100 rounded-xl">
+          <apexchart
+            type="line"
+            :series="lineChartSeries"
+            :options="lineChartOptions"
+          />
+        </div>
+        <div>
+            <label>FIRST LAYER</label>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date:</th>
+                        <th>Code No:</th>
+                        <th>Order No:</th>
+                        <th>Type:</th>
+                        <th>Lot No:</th>
+                        <th>Furnace No:</th>
+                        <th>Coating No:</th>
+                        <th>Zone:</th>
+                        <th>Pass No:</th>
+                        <th>Br:</th>
+                        <th>iHc:</th>
+                        <th>iHk:</th>
+                        <th>BHMax:</th>
+                        <th>Hr95:</th>
+                        <th>Hr98:</th>
+                        <th>iHciHk:</th>
+                        <th>Br:</th>
+                        <th>Br:</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+      </div>
+    </Frontend>
+  </template>
+
+  <script setup>
+    import Frontend from '@/Layouts/FrontendLayout.vue';
+    import { ref } from 'vue';
+
+    // Chart data for the Missing / Null Values chart
+    const lineChartSeries = ref([
+      {
+        name: 'Sample1',
+        data: [null, null, null, 15, 20, 20, 20, 20, 20, 20, 20, 20, 20, null, null, null, null, null, null, null, null, null, null],
+      },
+      {
+        name: 'Sample2',
+        data: [null, null, null, null, 12, 18, 18, 18, 18, 18, 18, 18, null, null, null, null, null, null, null, null, null, null, null],
+      },
+      {
+        name: 'Sample3',
+        data: [null, null, null, null, null, 9, 15, 15, 15, 15, 15, 15, null, null, null, null, null, null, null, null, null, null, null],
+      },
+    ]);
+
+    // Chart options including customizations like labels, stroke, and xaxis
+    const lineChartOptions = ref({
+      chart: {
+        height: 350,
+        type: 'line',
+        zoom: {
+          enabled: false,
+        },
+        animations: {
+          enabled: false,
+        },
+      },
+      stroke: {
+        width: [5, 5, 4],
+        curve: 'smooth',
+      },
+      labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
+      title: {
+        text: 'JH CURVE',
+        align: 'center',
+        style: {
+          fontSize: '18px',
+          fontWeight: 'bold',
+          color: '#333',
+        },
+      },
+      xaxis: {
+        // You can adjust any other specific x-axis properties here if necessary
+      },
+    });
+  </script>
---
+++ resources/js/Pages/Frontend/Reports hide.vue
@@ -0,0 +1,143 @@
+<template>
+    <Frontend>
+      <div class="flex flex-col items-center justify-center align-middle bg-gray-100 container-fluid">
+        <div>This is the Reports page</div>
+
+        <!-- Report Content -->
+            <div class="flex flex-col justify-center py-10 mx-20 my-20 align-middle bg-blue-100 shadow-2xl rounded-3xl">
+                <div class="flex flex-row mb-4 justify-evenly">
+                    <div class="flex flex-row items-baseline">
+                        <label class="text-sm font-semibold">Model:</label>&nbsp;
+                        <input type="text" name="pulseTracerMachineNo" class="w-[12rem] h-[1.5rem] p-2 mt-1 text-sm border rounded-md">
+                    </div>
+                    <div class="flex flex-row items-baseline">
+                        <label class="text-sm font-semibold">Material Code:</label>&nbsp;
+                        <input type="text" name="date" class="w-[12rem] h-[1.5rem] p-2 mt-1 text-sm border rounded-md">
+                    </div>
+                    <div class="flex flex-row items-baseline">
+                        <label class="text-sm font-semibold">Partial No.:</label>&nbsp;
+                        <input type="text" name="shift" class="w-[12rem] h-[1.5rem] p-2 mt-1 text-sm border rounded-md">
+                    </div>
+                    <div class="flex flex-row items-baseline">
+                        <label class="text-sm font-semibold">Total Quantity:</label>&nbsp;
+                        <input type="number" name="operator" class="w-[12rem] h-[1.5rem] p-2 mt-1 text-sm border rounded-md">
+                    </div>
+                </div>
+                <div class="flex flex-row mb-4 justify-evenly">
+                    <div class="flex flex-row items-baseline">
+                        <label class="text-sm font-semibold">Pulse Tracer Machine No:</label>&nbsp;
+                        <input type="text" name="pulseTracerMachineNo" class="w-[12rem] h-[1.5rem] p-2 mt-1 text-sm border rounded-md">
+                    </div>
+                    <div class="flex flex-row items-baseline">
+                        <label class="text-sm font-semibold">Date:</label>&nbsp;
+                        <input type="date" name="date" class="w-[12rem] h-[1.5rem] p-2 mt-1 text-sm border rounded-md">
+                    </div>
+                    <div class="flex flex-row items-baseline">
+                        <label class="text-sm font-semibold">Shift:</label>&nbsp;
+                        <input type="text" name="shift" class="w-[12rem] h-[1.5rem] p-2 mt-1 text-sm border rounded-md">
+                    </div>
+                    <div class="flex flex-row items-baseline">
+                        <label class="text-sm font-semibold">Operator:</label>&nbsp;
+                        <input type="text" name="operator" class="w-[12rem] h-[1.5rem] p-2 mt-1 text-sm border rounded-md">
+                    </div>
+                </div>
+
+                <div class="flex flex-row items-center justify-center align-middle">
+                    <table class="mt-[1.1rem] ml-[2rem] border-collapse table-auto">
+                        <thead>
+                            <tr class="bg-blue-300">
+                                <th colspan="2" class="px-2 py-2 text-lg font-semibold text-center">STANDARD&nbsp;SAMPLE&nbsp;DIMENSION&nbsp;and&nbsp;Material&nbsp;Grade<br>標準サンプル寸法および材料グレード</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="px-4 py-1 border">LENGTH&nbsp;(mm)</td>
+                                <td class="px-4 py-1 border"><input type="text" class="w-[10rem] py-1 text-sm border rounded-md"></td>
+                            </tr>
+                            <tr>
+                                <td class="px-4 py-1 border">WIDTH&nbsp;(mm)</td>
+                                <td class="px-4 py-1 border"><input type="text" class="w-[10rem] py-1 text-sm border rounded-md"></td>
+                            </tr>
+                            <tr>
+                                <td class="px-4 py-1 border">THICKNESS&nbsp;(mm)</td>
+                                <td class="px-4 py-1 border "><input type="text" class="w-[10rem] py-1 text-sm border rounded-md"></td>
+                            </tr>
+                            <tr>
+                                <td class="px-4 py-1 border">MATERIAL&nbsp;GRADE</td>
+                                <td class="px-4 py-1 border"><input type="text" class="w-[10rem] py-1 text-sm border rounded-md"></td>
+                            </tr>
+                            <tr>
+                                <td class="px-4 py-1 border">MPI&nbsp;SAMPLE&nbsp;QTY.</td>
+                                <td class="px-4 py-1 border"><input type="text" class="w-[10rem] py-1 text-sm border rounded-md"></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table class="ml-2 mt-[1rem] mr-[2rem] border-collapse table-auto">
+                        <thead>
+                            <tr class="bg-blue-300">
+                                <th colspan="6" class="py-4 text-lg font-semibold text-center">MAGNETIC&nbsp;PROPERTY&nbsp;DATA(磁気特性データ)</th>
+                            </tr>
+                            <tr class="bg-blue-200">
+                                <th rowspan="2" class="px-4 border">ITEMS</th>
+                                <th rowspan="2" class="px-4 border">STANDARD</th>
+                                <th colspan="4" class="px-4 text-center border">ACTUAL DATA</th>
+                            </tr>
+                            <tr class="bg-blue-200">
+                                <th class="px-4 border">AVERAGE</th>
+                                <th class="px-4">MAXIMUM</th>
+                                <th class="px-4">MINIMUM</th>
+                                <th class="px-4">VARIANCE<br>(max-min)</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="px-4 py-2 border">Br (G)</td>
+                                <td class="px-4 py-2 border"><input type="text" class="w-full px-2 py-1 text-sm border rounded-md"></td>
+                                <td class="px-4 py-2 border"><input type="text" class="w-full px-2 py-1 text-sm border rounded-md"></td>
+                                <td class="px-4 py-2 border"><input type="text" class="w-full px-2 py-1 text-sm border rounded-md"></td>
+                                <td class="px-4 py-2 border"><input type="text" class="w-full px-2 py-1 text-sm border rounded-md"></td>
+                                <td class="px-4 py-2 border"><input type="text" class="w-full px-2 py-1 text-sm border rounded-md"></td>
+                            </tr>
+                            <tr>
+                                <td class="px-4 py-2 border">iHc (Oe)</td>
+                                <td class="px-4 py-2 border"><input type="text" class="w-full px-2 py-1 text-sm border rounded-md"></td>
+                                <td class="px-4 py-2 border"><input type="text" class="w-full px-2 py-1 text-sm border rounded-md"></td>
+                                <td class="px-4 py-2 border"><input type="text" class="w-full px-2 py-1 text-sm border rounded-md"></td>
+                                <td class="px-4 py-2 border"><input type="text" class="w-full px-2 py-1 text-sm border rounded-md"></td>
+                                <td class="px-4 py-2 border"><input type="text" class="w-full px-2 py-1 text-sm border rounded-md"></td>
+                            </tr>
+                            <tr>
+                                <td class="px-4 py-2 border">iHk (Oe)</td>
+                                <td class="px-4 py-2 border"><input type="text" class="w-full px-2 py-1 text-sm border rounded-md"></td>
+                                <td class="px-4 py-2 border"><input type="text" class="w-full px-2 py-1 text-sm border rounded-md"></td>
+                                <td class="px-4 py-2 border"><input type="text" class="w-full px-2 py-1 text-sm border rounded-md"></td>
+                                <td class="px-4 py-2 border"><input type="text" class="w-full px-2 py-1 text-sm border rounded-md"></td>
+                                <td class="px-4 py-2 border"><input type="text" class="w-full px-2 py-1 text-sm border rounded-md"></td>
+                            </tr>
+                            <tr class="bg-blue-200">
+                                <th rowspan="2" class="px-4 border">Br Cpk</th>
+                                <th class="px-4 border">STD DEV</th>
+                                <th class="px-4 border">Cpu</th>
+                                <th class="px-4 border">Cpl</th>
+                                <th class="px-4 border">Cpk</th>
+                                <th class="px-4 border">Remarks</th>
+                            </tr>
+                            <tr>
+                                <td class="px-4 py-2 border"><input type="text" class="w-full px-2 py-1 text-sm border rounded-md"></td>
+                                <td class="px-4 py-2 border"><input type="text" class="w-full px-2 py-1 text-sm border rounded-md"></td>
+                                <td class="px-4 py-2 border"><input type="text" class="w-full px-2 py-1 text-sm border rounded-md"></td>
+                                <td class="px-4 py-2 border"><input type="text" class="w-full px-2 py-1 text-sm border rounded-md"></td>
+                                <td class="px-4 py-2 border"><input type="text" class="w-full px-2 py-1 text-sm border rounded-md"></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+      </div>
+    </Frontend>
+</template>
+
+<script setup>
+import Frontend from '@/Layouts/FrontendLayout.vue';
+</script>
