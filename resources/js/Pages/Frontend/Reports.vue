6cd7c55d13f49ff7f261ee8315fa086ce5b3d4fd
bellosillo137847 <jbellosillo.137847@gmail.com>
2025-03-25 10:04:09 +0800

    test push

--- resources/js/Pages/Frontend/Reports.vue
+++ resources/js/Pages/Frontend/Reports.vue
@@ -5,6 +5,12 @@
 
         <div>
             <p>Select Serial No: </p>
+            <select>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+            </select>
         </div>
 
         <!-- Report Content -->
