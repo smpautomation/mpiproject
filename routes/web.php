3f7dee1a105c0e8192219e5bbe48e60a5a0ce0e3
bellosillo137847 <jbellosillo.137847@gmail.com>
2025-06-08 20:21:17 +0800

    commit before push

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\PdfController;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
 use App\Mail\TestMail;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Http\Request;
