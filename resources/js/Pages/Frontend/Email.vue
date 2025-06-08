f997051bfcdd22b5e5c2f90b40293af05e9d9e5f
bellosillo137847 <jbellosillo.137847@gmail.com>
2025-06-09 03:26:58 +0800

    test commit

--- resources/js/Pages/Frontend/Email.vue
+++ resources/js/Pages/Frontend/Email.vue
@@ -63,6 +63,58 @@ import Frontend from '@/Layouts/FrontendLayout.vue';
 import { reactive, ref, onMounted, nextTick, watch } from 'vue';
 import { Inertia } from '@inertiajs/inertia';
 import DotsLoader from '@/Components/DotsLoader.vue';
+import { useAuth } from '@/Composables/useAuth.js'
+
+const { state } = useAuth();
+
+// Function to check authentication
+const checkAuthentication = async () => {
+    try {
+
+        const start = Date.now();
+        const timeout = 500; // 5 seconds
+
+        while (!state.user) {
+            if (Date.now() - start > timeout) {
+                console.error('Auth timeout: user data failed to load within 5 seconds.');
+                Inertia.visit('/'); // Redirect if not authenticated
+                return false;
+            }
+            await new Promise(resolve => setTimeout(resolve, 50)); // small delay
+        }
+
+        if (!state.isAuthenticated) {
+            Inertia.visit('/'); // Redirect if not authenticated
+
+            return false; // Indicate not authenticated
+        }
+
+        console.warn("USER AUTHENTICATED!");
+        console.warn("Name: ", state.user.firstName + " " + state.user.surname);
+        console.warn("Access: ", state.user.access_type);
+
+        return true; // Indicate authenticated
+    } catch (error) {
+        console.error('Error checking authentication:', error);
+        Inertia.visit('/'); // Redirect on error
+        return false; // Indicate not authenticated
+    }
+};
+
+
+const userEmailLogging = async (logEvent) => {
+    try{
+        const responseEmailLogging = await axios.post('/api/userlogs', {
+            user: state.user.firstName + " " + state.user.surname,
+            event: logEvent,
+            section: 'Email',
+        });
+
+        //console.log('responseEmailLogging-data: ',responseEmailLogging.data);
+    }catch(error){
+        console.error('userEmailLogging post request failed: ',error);
+    }
+}
 
 const form = reactive({
     massPro: '',
@@ -87,6 +139,8 @@ loading.value = true;
         success.value = false;
     }, 3000); // 3000 milliseconds = 3 seconds
 
+    await userEmailLogging(`has successfully sent an email for ${form.massPro}`);
+
     // Reset form
     form.massPro = ''
     form.emails = ''
@@ -104,4 +158,9 @@ loading.value = true;
     loading.value = false;
   }
 }
+
+onMounted(async()=>{
+    await checkAuthentication();
+});
+
 </script>
