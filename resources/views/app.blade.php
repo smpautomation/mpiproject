f8083f701e021e59767045f071e7a13ea641dff8
bellosillo137847 <jbellosillo137847@gmail.com>
2025-06-17 18:36:44 +0800

    removed fonts on app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,9 +6,10 @@
 
         <title inertia></title>
 
-        <!-- Fonts -->
+        <!-- Fonts
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        -->
 
         <!-- Scripts -->
         @routes
