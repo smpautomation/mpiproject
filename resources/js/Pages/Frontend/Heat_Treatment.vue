52406f4bdf76dcd25923cd4e8725e87529caca7e
bellosillo137847 <jbellosillo.137847@gmail.com>
2026-02-05 03:31:27 +0800

    fixed bug, 1st and 2nd gbdp cannot encode layer data

--- resources/js/Pages/Frontend/Heat_Treatment.vue
+++ resources/js/Pages/Frontend/Heat_Treatment.vue
@@ -1125,7 +1125,7 @@
                                     {{
                                         noMassProdData
                                             ? 'NO MASS PROD DATA'
-                                            : isExisting || isExisting_2ndGBDP || isExistingExcess
+                                            : isExisting || isExistingExcess
                                                 ? 'LAYER OCCUPIED'
                                                 : !isDataShown
                                                     ? 'SHOW DATA FIRST'
@@ -1175,7 +1175,7 @@
                             <svg class="w-5 h-5 transition-transform group-hover:scale-110 drop-shadow-md" fill="currentColor" viewBox="0 0 20 20">
                             <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z" clip-rule="evenodd" />
                             </svg>
-                            <span class="drop-shadow-md">APPLY 1ST 2ND GBDP</span>
+                            <span class="drop-shadow-md">{{ isExisting_2ndGBDP ? 'DATA ALREADY EXIST' : ' APPLY 1ST 2ND GBDP' }}</span>
                         </span>
                         </button>
 
