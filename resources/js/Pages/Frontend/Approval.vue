3671a2c9a3071d1b1fcad962d5bec7bda7666606
Automation Engr <automation3@smp.local>
2026-05-24 14:12:47 +0800

    [System Update] Adjust items per page to 50 on Approval.vue

--- resources/js/Pages/Frontend/Approval.vue
+++ resources/js/Pages/Frontend/Approval.vue
@@ -450,7 +450,7 @@ watch(statusFilter, async (value) => {
 });
 
 const currentPage = ref(1);
-const itemsPerPage = ref(2); // You can change to 20, 50, etc.
+const itemsPerPage = ref(50); // You can change to 20, 50, etc.
 
 const showApproveButton = ref(true);
 const showApproveConfirmation = ref(false);
